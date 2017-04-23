2a20c9313cf8e48a166c58e890b52ac460f01192
Lonmee <Lonmee@126.com>
2017-04-23 20:31:54 +0800

    From Lonmee-DESKTOP

--- ts/Gear.ts
+++ ts/Gear.ts
@@ -1,13 +1,15 @@
 /**
  * Created by Lonmee on 4/19/2017.
  */
-import {ViewMgr} from "./view/ViewMgr";
 
 class Gear {
     constructor() {
+        require(["../../verdor/laya/laya.core"], this.engReady);
+    }
+
+    engReady() {
         let canvas = Laya.init(800, 600);
         document.body.appendChild(canvas);
-        Laya.stage.addChild(new ViewMgr());
     }
 }
 
