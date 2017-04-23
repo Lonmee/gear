db081d871f44bbd066b9ddf918ccbb2f80506132
Lonmee <Lonmee@126.com>
2017-04-23 16:06:05 +0800

    From Lonmee-DESKTOP

--- ts/Gear.ts
+++ ts/Gear.ts
@@ -1,40 +1,27 @@
 /**
  * Created by Lonmee on 4/19/2017.
  */
-import {Person} from "./data/Person";
-import {Person as NPCPerson} from "./data/npc/Person";
-import {Story, Chapter, Scene} from "./data/Data";
+import {Story} from "./data/Data";
 
 class Gear {
-    private person: Person;
     private story: Story;
-    private chapter: Chapter;
-    private scene: Scene;
-    private str: number;
 
     constructor() {
-        this.str = 9;
-        this.person = new Person("Lunar");
-        console.log(this.person.name);
-        this.person.walk();
+        //require(["xx"], this.load);
 
         this.story = new Story(0);
-        console.log(`story ID: ${this.story.id}`);
+        let c = this.story.chapters[0];
+        let p = this.story.person;
 
-        this.chapter = new Chapter(1);
-        console.log(`chapter ID: ${this.story.id}`);
-
-        this.scene = new Scene(3);
-        console.log(`scene ID: ${this.scene.id}`);
-
-        let p: NPCPerson = new NPCPerson(55);
-        console.log(p.age);
     }
 
     init() {
 
     }
 
+    load() {
+
+    }
 }
 
 new Gear().init();
--- ts/data/Data.ts
+++ ts/data/Data.ts
@@ -1,9 +1,15 @@
+import {Person} from "./npc/Person";
 /**
  * Created by ShanFeng on 4/19/2017.
  */
 
 export class Story {
+    person: Person;
+    chapters: Array<Chapter>;
     constructor(private _id: number) {
+        this.chapters = new Array();
+        this.chapters.push(new Chapter(2));
+        this.person = new Person(88);
     }
 
     get id(): number {
