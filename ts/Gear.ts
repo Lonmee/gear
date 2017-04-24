d0b95c0b2faf71704626b61f451173e422360c31
Lonmee <Lonmee@126.com>
2017-04-24 09:56:19 +0800

    from Working PC

--- ts/Gear.ts
+++ ts/Gear.ts
@@ -2,14 +2,15 @@
  * Created by Lonmee on 4/19/2017.
  */
 import {ViewMgr} from "./view/ViewMgr";
+import WebGL = laya.webgl.WebGL;
 class Gear {
     constructor() {
-        let canvas = Laya.init(Laya.Browser.width, Laya.Browser.height);
+        let canvas = Laya.init(Laya.Browser.width, Laya.Browser.height, WebGL);
         document.body.appendChild(canvas);
-        // Laya.stage.addChild(new ViewMgr());
-        require(["js/view/SmartScale_T"], function () {
-            new laya.SmartScale_T();
-        })
+        Laya.stage.addChild(new ViewMgr());
+        // require(["js/view/SmartScale_T"], function () {
+            // new laya.SmartScale_T();
+        // })
     }
 }
 
