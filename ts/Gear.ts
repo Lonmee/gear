7e3ac85a69036df0964f518188e0031a0516d6df
Lonmee <Lonmee@126.com>
2017-04-24 22:39:13 +0800

    Frome Lonmee's Air

--- ts/Gear.ts
+++ ts/Gear.ts
@@ -3,14 +3,41 @@
  */
 import {ViewMgr} from "./view/ViewMgr";
 import WebGL = laya.webgl.WebGL;
+import Handler = laya.utils.Handler;
+import Loader = laya.net.Loader;
 class Gear {
     constructor() {
         let canvas = Laya.init(Laya.Browser.width, Laya.Browser.height, WebGL);
         document.body.appendChild(canvas);
         Laya.stage.addChild(new ViewMgr());
         // require(["js/view/SmartScale_T"], function () {
-            // new laya.SmartScale_T();
+        // new laya.SmartScale_T();
         // })
+        Laya.loader.load("http://www.lonmee.com/favicon.ico",
+            Handler.create(this, this.completeHandler, null, true),
+            Handler.create(this, this.progressHandler, null, true),
+            Loader.IMAGE,
+            0,
+            true,
+            "bin",
+            false
+        );
+        //load(url: any,
+        // complete?: laya.utils.Handler,
+        // progress?: laya.utils.Handler,
+        // type?: string,
+        // priority?: number,
+        // cache?: boolean,
+        // group?: string,
+        // ignoreCache?: boolean): LoaderManager;
+    }
+
+    progressHandler(e) {
+        e;
+    }
+
+    completeHandler(e) {
+        e;
     }
 }
 
